07d9d6688b40581737391c9b92dfc52d73dddadd
Рыжов Алексей Денисович <ryzhov.aleksey4@wildberries.ru>
2020-01-21 16:12:12 +0300

    Update 16:12

--- pkg/facade/facade.go
+++ pkg/facade/facade.go
@@ -4,61 +4,61 @@ package facade
 import "strings"
 
 // Implements user
-type User struct {
-	wallet *Wallet
-	status *Status
-	pay    *Pay
+type user struct {
+	wallet *wallet
+	status *status
+	pay    *pay
 }
 
 // Wallet ...
-type Wallet struct {
+type wallet struct {
 	money int
 }
 
 // Status ...
-type Status struct {
+type status struct {
 	text string
 }
 
 // Pay ...
-type Pay struct {
+type pay struct {
 	sum int
 }
 
 // Create wallet and put in 15000 y.e.
-func (w *Wallet) Create() string {
+func (w *wallet) create() string {
 	w.money = 15000
 	return "Wallet was create."
 }
 
 // Install auto-status
-func (s *Status) GetStatus() string {
+func (s *status) getStatus() string {
 	s.text = "Nice day! :)"
 	return "Status: " + s.text
 }
 
 // Initial first pay
-func (p *Pay) PayToSite(w *Wallet) string {
+func (p *pay) payToSite(w *wallet) string {
 	p.sum = 1000
 	w.money = w.money - p.sum
 	return "Pay was success."
 }
 
 // FirstSignIn returns user's data after first sign in.
-func (u *User) FirstSignIn() string {
+func (u *user) FirstSignIn() string {
 	result := []string{
-		u.wallet.Create(),
-		u.status.GetStatus(),
-		u.pay.PayToSite(u.wallet),
+		u.wallet.create(),
+		u.status.getStatus(),
+		u.pay.payToSite(u.wallet),
 	}
 	return strings.Join(result, "\n")
 }
 
 // NewUser creates user.
-func NewUser() *User {
-	return &User{
-		wallet: &Wallet{},
-		status: &Status{},
-		pay:    &Pay{},
+func NewUser() *user {
+	return &user{
+		wallet: &wallet{},
+		status: &status{},
+		pay:    &pay{},
 	}
 }
