5a40456af2b27bac752efa9e5fd7d9096d389151
Рыжов Алексей Денисович <ryzhov.aleksey4@wildberries.ru>
2020-01-22 18:01:54 +0300

    Second Builder

--- Task2/cmd/builder/main.go
+++ Task2/cmd/builder/main.go
@@ -2,16 +2,15 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/RyzhAlexWork/go-intern/Task2/pkg/builder"
 )
 
 func main() {
 	build := builder.NewBuild()
-	director := builder.NewDirector(build, build.BuildCar())
-	car1 := director.ConstructSportcar()
-	car2 := director.ConstructTank()
+	car := build.BuildCar()
+	director := builder.NewDirector(build, car)
+	car = director.ConstructSportcar()
 
-	fmt.Println(car1.ShowCar())
-	fmt.Println(car2.ShowCar())
-	fmt.Println(car1.ShowCar())
+	fmt.Println(car.ShowCar())
 }
