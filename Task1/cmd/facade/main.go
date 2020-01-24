281b48ac1346d2b1979d78220880843269b8ba8a
Рыжов Алексей Денисович <ryzhov.aleksey4@wildberries.ru>
2020-01-24 13:23:29 +0300

    Task1 24.01.2020 13:24

--- Task1/cmd/facade/main.go
+++ Task1/cmd/facade/main.go
@@ -2,38 +2,20 @@ package main
 
 import (
 	"fmt"
-	"github.com/RyzhAlexWork/go-intern/Task1/pkg/models"
-
 	"github.com/RyzhAlexWork/go-intern/Task1/pkg/facade"
 	"github.com/RyzhAlexWork/go-intern/Task1/pkg/status"
 	"github.com/RyzhAlexWork/go-intern/Task1/pkg/wallet"
-)
-
-var (
-	expect = []models.Status{
-		"Deposit was successful.",
-		"Pay was successful.",
-	}
-	result  models.Status
-	balance int
 )
 
 func main() {
 	newWallet := wallet.NewWallet(0, 1000000)
 	newStatus := walletstatus.NewWalletStatus()
 	newUser := facade.NewUser("Aksel", newWallet, newStatus)
-	result = newUser.Add(500000)
-	if result != expect[0] {
-		fmt.Printf("Expect result to equal: %s, but: %s.\n", expect, result)
-	}
-	result = newUser.Pay(200000)
-	if result != expect[1] {
-		fmt.Printf("Expect result to equal: %s, but: %s.\n", expect, result)
-	}
-	balance = newUser.Balance()
-	if balance != 300000 {
-		fmt.Printf("Wrong balance! Expect: 300000, balance: %d", balance)
-	} else {
-		fmt.Println("All ok!")
-	}
+
+	status := newUser.Add(500000)
+	fmt.Printf("%s\n", status)
+	status = newUser.Pay(200000)
+	fmt.Printf("%s\n", status)
+	balance := newUser.Balance()
+	fmt.Printf("Balance: %d\n", balance)
 }
--- Task1/pkg/facade/facade_test.go
+++ Task1/pkg/facade/facade_test.go
@@ -1,6 +1,7 @@
 package facade
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 
 	walletstatusPackage "github.com/RyzhAlexWork/go-intern/Task1/pkg/status"
@@ -13,24 +14,23 @@ var (
 	expectsAdd     = []int{1000, 16000, 516000, 516000}
 	expectsPay     = []int{511000, 211000, 211000}
 	balance        int
-	newWallet      = walletPackage.NewWallet(0, 1000000)
-	newStatus      = walletstatusPackage.NewWalletStatus()
 )
 
 func Test_Facade(t *testing.T) {
+	newWallet := walletPackage.NewWallet(0, 1000000)
+	newStatus := walletstatusPackage.NewWalletStatus()
 	newUser := NewUser("Valera", newWallet, newStatus)
-	for i, expect := range expectsAdd {
-		newUser.Add(addToBalance[i])
-		balance = newUser.Balance()
-		if balance != expect {
-			t.Errorf("Wrong balance after deposit! Expect: %d, balance: %d", expect, balance)
+
+	t.Run("facadeTest", func(t *testing.T) {
+		for i, expect := range expectsAdd {
+			newUser.Add(addToBalance[i])
+			balance = newUser.Balance()
+			assert.Equal(t, expect, balance)
 		}
-	}
-	for i, expect := range expectsPay {
-		newUser.Pay(payFromBalance[i])
-		balance = newUser.Balance()
-		if balance != expect {
-			t.Errorf("Wrong balance after pay! Expect: %d, balance: %d", expect, balance)
+		for i, expect := range expectsPay {
+			newUser.Pay(payFromBalance[i])
+			balance = newUser.Balance()
+			assert.Equal(t, expect, balance)
 		}
-	}
+	})
 }
